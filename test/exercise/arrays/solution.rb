990a6cf77b65cf090206f589ee0911cdd49afc60
Sergey Polovchenya <sergey.polovchenya@gmail.com>
2023-06-07 11:31:04 +0300

    Implemented methods for arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,26 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max_element = array[0]
+        1.upto(array.length - 1) do |i|
+          max_element = array[i] if array[i] > max_element
+        end
+        array.map do |element|
+          element.positive? ? max_element : element
+        end
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query, low_index = 0, high_index = array.size - 1)
+        return -1 if low_index > high_index
+
+        middle_index = (low_index + high_index) / 2
+        return middle_index if query == array[middle_index]
+
+        if query < array[middle_index]
+          search(array, query, low_index, middle_index - 1)
+        else
+          search(array, query, middle_index + 1, high_index)
+        end
       end
     end
   end
