578358b4e7efc72a82cd5886f917b5aaf4568887
Sergey Polovchenya <sergey.polovchenya@gmail.com>
2023-06-08 23:43:17 +0300

    Using the MapReduce model

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,12 +4,22 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        sorted_array = array.map do |film|
+          film if film['country'].to_s.split(',').size >= 2 && film['rating_kinopoisk'].to_f.positive?
+        end
+
+        sorted_array.compact!
+        sorted_array.reduce(0) { |sum, film| sum + film['rating_kinopoisk'].to_f } / sorted_array.size
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        sorted_array = films.map do |film|
+          film if film['rating_kinopoisk'].to_f >= threshold
+        end
+
+        sorted_array.compact!
+        sorted_array.reduce(0) { |sum, film| sum + film['name'].count('и') }
       end
     end
   end
